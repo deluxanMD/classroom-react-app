fd14d0eabb8ca15cca69688ac5f8d78075809796
deluxanMD <deluxan.mariathasan@gmail.com>
2022-10-28 16:02:00 +0530

    app component test fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div data-testid="App.Container"></div>;
 };
 
 export default App;
